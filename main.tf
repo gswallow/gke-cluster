b6b87db86ce268a91ca36925597b3af0a5182423
Greg Swallow <gswallow@developertown.com>
2020-11-17 18:39:30 -0500

    Use Google's submodules to enable private nodes and use beta features that are now avialable.
    I'm confused why this isn't just part of the default module.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "random_id" "kms" {
 }
 
 resource "google_project_iam_member" "k8s_developer" {
-  provider = "google-beta"
+  provider = google-beta
   count    = length(var.k8s_developers)
   role     = "roles/container.developer"
   project  = data.google_project.project.project_id
@@ -11,7 +11,7 @@ resource "google_project_iam_member" "k8s_developer" {
 }
 
 resource "google_project_iam_member" "kms_encrypter_decrypter" {
-  provider   = "google-beta"
+  provider   = google-beta
   project    = data.google_project.project.project_id
   role       = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member     = "serviceAccount:service-${data.google_project.project.number}@container-engine-robot.iam.gserviceaccount.com"
@@ -34,29 +34,31 @@ resource "google_kms_crypto_key" "etcd" {
   }
 }
 
-
-
 # impersonate_service_account is not yet released, so use a ref spec.
 module "kubernetes-engine" {
-  source                      = "github.com/terraform-google-modules/terraform-google-kubernetes-engine?ref=685a2db99f5943c2f74f931cde6923e596896d02"
-  project_id                  = var.project_id
-  name                        = var.cluster_name
-  description                 = var.cluster_description
-  kubernetes_version          = var.kubernetes_version#
-  region                      = var.gcp_region
-  zones                       = data.google_compute_zones.available.names
-  network                     = var.network_name
-  network_project_id          = var.network_project_id
-  subnetwork                  = local.chosen_subnetwork
-  ip_range_pods               = "gke-pods"
-  ip_range_services           = "gke-services"
-  node_pools                  = var.node_pools
-  add_cluster_firewall_rules  = var.add_cluster_firewall_rules
-  cluster_autoscaling         = var.cluster_autoscaling
-  cluster_resource_labels     = var.labels
-  create_service_account      = var.create_service_account
-  impersonate_service_account = var.terraform_service_account
-  database_encryption = [{
+  source                             = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster"
+  project_id                         = var.project_id
+  name                               = var.cluster_name
+  description                        = var.cluster_description
+  kubernetes_version                 = var.kubernetes_version#
+  region                             = var.gcp_region
+  zones                              = data.google_compute_zones.available.names
+  network                            = var.network_name
+  network_project_id                 = var.network_project_id
+  subnetwork                         = local.chosen_subnetwork
+  enable_pod_security_policy         = var.enable_pod_security_policy
+  enable_private_nodes               = var.enable_private_nodes
+  enable_vertical_pod_autoscaling    = var.enable_vertical_pod_autoscaling
+  ip_range_pods                      = "gke-pods"
+  ip_range_services                  = "gke-services"
+  add_cluster_firewall_rules         = var.add_cluster_firewall_rules
+  cloudrun                           = var.cloudrun
+  cloudrun_load_balancer_type        = var.cloudrun_load_balancer_type
+  cluster_autoscaling                = var.cluster_autoscaling
+  cluster_resource_labels            = var.labels
+  create_service_account             = var.create_service_account
+  impersonate_service_account        = var.terraform_service_account
+  database_encryption                = [{
     state    = "ENCRYPTED"
     key_name = google_kms_crypto_key.etcd.id
   }]
@@ -65,11 +67,16 @@ module "kubernetes-engine" {
   enable_network_egress_export       = var.enable_network_egress_export
   enable_resource_consumption_export = var.enable_resource_consumption_export
   enable_shielded_nodes              = var.enable_shielded_nodes
+  gce_pd_csi_driver                  = var.gce_pd_csi_driver
   grant_registry_access              = var.grant_registry_access
   horizontal_pod_autoscaling         = var.horizontal_pod_autoscaling
   http_load_balancing                = var.http_load_balancing
+  istio                              = var.istio
+  istio_auth                         = var.istio_auth
   logging_service                    = var.logging_service
   maintenance_start_time             = var.maintenance_start_time
+  maintenance_recurrence             = var.maintenance_recurrence
+  maintenance_end_time               = var.maintenance_end_time
   master_authorized_networks         = var.master_authorized_networks
   monitoring_service                 = var.monitoring_service
   network_policy                     = var.network_policy
@@ -80,4 +87,7 @@ module "kubernetes-engine" {
   service_account                    = var.service_account
   stub_domains                       = var.stub_domains
   upstream_nameservers               = var.upstream_nameservers
+
+  node_pools                         = var.node_pools
+  node_pools_oauth_scopes            = var.node_pools_oauth_scopes
 }
--- variables.tf
+++ variables.tf
@@ -64,17 +64,32 @@ variable "add_cluster_firewall_rules" {
   default     = true
 }
 
+variable "cloudrun" {
+  description = "Enable Cloud Run" 
+  type        = bool
+  default     = false
+}
+
+variable "cloudrun_load_balancer_type" {
+  description = "Configure the Cloud Run load balancer type. External by default. Set to LOAD_BALANCER_TYPE_INTERNAL to configure as an internal load balancer"
+  type        = string
+  default     = ""
+}
+
 variable "cluster_autoscaling" {
   description = "Enable cluster autoscaling"
   type = object({
     enabled       = bool
+    autoscaling_profile = string
     min_cpu_cores = number
     max_cpu_cores = number
     min_memory_gb = number
     max_memory_gb = number
+
   })
   default = {
     enabled       = false
+    autoscaling_profile = "BALANCED"
     max_cpu_cores = 0
     min_cpu_cores = 0
     max_memory_gb = 0
@@ -91,7 +106,7 @@ variable "create_service_account" {
 variable "default_max_pods_per_node" {
   description = "The maximum number of pods to run per node."
   type        = number
-  default     = 110
+  default     = 16
 }
 
 variable "enable_binary_authorization" {
@@ -106,6 +121,18 @@ variable "enable_network_egress_export" {
   default     = false
 }
 
+variable "enable_pod_security_policy" {
+  description = "If enabled, pods must be valid under a PodSecurityPolicy to be created"
+  type        = bool
+  default     = false
+}
+
+variable "enable_private_nodes" {
+  description = "Whether nodes have internal IP addresses only"
+  type        = bool
+  default     = true
+}
+
 variable "enable_resource_consumption_export" {
   description = "When enabled, a table will be created in the resource export BigQuery dataset to store resource consumption data"
   type        = bool
@@ -118,10 +145,22 @@ variable "enable_shielded_nodes" {
   default     = true
 }
 
+variable "enable_vertical_pod_autoscaling" {
+  description = "Scale pods vertically (more RAM and CPU) depending on demand"
+  type        = bool
+  default     = false
+}  
+
 variable "firewall_inbound_ports" {
   description = "List of TCP ports for admission/webhook controllers"
   type        = list(string)
   default     = []
+}
+
+variable "gce_pd_csi_driver" {
+  description = "Whether this cluster should enable the Google Compute Engine Persistent Disk Container Storage Interface (CSI) Driver"
+  type        = bool
+  default     = true
 }
 
 variable "grant_registry_access" {
@@ -133,13 +172,25 @@ variable "grant_registry_access" {
 variable "horizontal_pod_autoscaling" {
   description = "Enable horizontal pod autoscaling addon"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "http_load_balancing" {
   description = "Enable HTTP load balancer addon"
   type        = bool
   default     = true
+}
+
+variable "istio" {
+  description = "Enable Istio"
+  type        = bool
+  default     = false
+}
+
+variable "istio_auth" {
+  description = "The authentication type between services in Istio"
+  type        = string
+  default     = "AUTH_MUTUAL_TLS"
 }
 
 variable "logging_service" {
@@ -151,7 +202,19 @@ variable "logging_service" {
 variable "maintenance_start_time" {
   description = "Time window for daily or recurring maintenance operations"
   type        = string
-  default     = "05:00"
+  default     = "2006-01-02T02:00:00Z"
+}
+
+variable "maintenance_recurrence" {
+  description = "Frequency of the recurring maintenance window in RFC 5545 format"
+  type        = string
+  default     = "FREQ=WEEKLY;BYDAY=SA,SU"
+}
+
+variable "maintenance_end_time" {
+  description = "The end of the window for daily or recurring maintenance operations"
+  type        = string
+  default     = "2006-01-02T08:00:00Z"
 }
 
 variable "master_authorized_networks" {
@@ -183,20 +246,40 @@ variable "node_pools" {
   type        = list(map(string))
   default     = [
     {
-      name               = "default-node-pool"
-      machine_type       = "e2-medium"
-      min_count          = 1
-      max_count          = 10
-      local_ssd_count    = 0
-      disk_size_gb       = 100
-      disk_type          = "pd-standard"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
-      preemptible        = false
-      initial_node_count = 1
+      name                 = "default-node-pool"
+      machine_type         = "e2-medium"
+      min_count            = 1
+      max_count            = 5
+      local_ssd_count      = 0
+      disk_size_gb         = 100
+      disk_type            = "pd-standard"
+      image_type           = "COS"
+      auto_repair          = true
+      auto_upgrade         = true
+      preemptible          = false
+      enable_secure_boot   = true
+      max_surge            = 2
+      max_unavailable      = 0
+      initial_node_count   = 1
     },
   ]
+}
+
+variable "node_pools_oauth_scopes" {
+  type        = map(list(string))
+  description = "Map of lists containing node oauth scopes by node-pool name"
+  default     = {
+    all = []
+
+    default-node-pool = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
 }
 
 variable "registry_project_id" {
